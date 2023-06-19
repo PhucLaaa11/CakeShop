090ce6f981fbba89454c3095b118d3848640fbbf
Viet Tien <tientvgcc210283@fpt.edu.vn>
2023-06-19 17:30:19 +0700

    Supplier index

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -12,6 +12,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SupplierController extends AbstractController
 {
+    #[Route('/supplier', name: 'app_supplier')]
+    public function index(): Response
+    {
+        return $this->render('supplier/index.html.twig', [
+            'Supplier' => 'SupplierController',
+        ]);
+    }
     #[Route('/supplier/create', name: 'app_supplier_create', priority: 1)]
     public function createAction(SupplierRepository $supplierRepository, Request $request): Response
     {
