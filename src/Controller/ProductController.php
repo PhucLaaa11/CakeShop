de8ec6c59f41fbc0b786ae43a78edc8d619c6803
Phuc La <phuclhgcc210353@fpt.edu.vn>
2023-06-19 15:22:35 +0700

    Product create

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/product/create', name: 'app_product_create', priority: 1)]
+    public function createAction(ProductRepository $productRepository, Request $request): Response
+    {
+
+        $form = $this->createForm(ProductType::class, new  Product());
+        //dd($request);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+            $productRepository->save($product, true);
+            $this->addFlash('success', 'Product\'s inserted successfully');
+            return $this->redirectToRoute('app_product_all');
+        }
+
+        return $this->render('product/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+    #[Route('/product/all', name: 'app_product_all')]
+    public function getProduct(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/index.html.twig',
+            ['products' => $products]);
+    }
+}
---
+++ src/Form/ProductType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Supplier;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,['label_attr'=>['class'=>'form-label'], 'attr'=>['class'=>'form-control']])
+            ->add('maintaste', TextType::class,['label_attr'=>['class'=>'form-label'], 'attr'=>['class'=>'form-control']])
+            ->add('price', TextType::class,['label_attr'=>['class'=>'form-label'], 'attr'=>['class'=>'form-control']])
+            ->add('count', TextType::class,['label_attr'=>['class'=>'form-label'], 'attr'=>['class'=>'form-control']])
+            ->add('supplier', EntityType::class, [
+                "class" => Supplier::class,
+                "query_builder" => function(EntityRepository $er)
+                {
+                    return $er->createQueryBuilder('s')
+                        ->orderBy('s.name');
+                },
+                "choice_label" => 'name',
+                "multiple"=>false,
+                'label_attr' => ['class' => 'form-label'],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('submit', SubmitType::class, ['attr' => ['class' => 'btn btn-success']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
